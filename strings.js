56fcf1bcec67a6134c9f427b0807aa02ea73d045
DercilioFontes <dercilioafontes@gmail.com>
2018-04-12 19:00:16 -0700

    Finished Strings (Chapter 3).

--- strings.js
+++ strings.js
@@ -6,11 +6,11 @@ const imperative = "Don't do that";
   const dialog = "Sam looked up and said "don't do that!" to Max.";
 */
 
-// Quote escape
+// # Quote escape
 const dialog1 = "He looked up and said \"don't do that!\" to Max.";
 const dialog2 = 'He looked up and said "don\'t do that!" to Max';
 
-// Backslash escape
+// # Backslash escape
 const s = "In JavaScript, use \\ as an escape character in strings.";
 
 /*
@@ -19,7 +19,7 @@ const s = "In JavaScript, use \\ as an escape character in strings.";
  - Single quotation marks:  When expressing HTML inside a JavaScript string, so that we can use double quotation marks for attribute values.
 */
 
-// Special Characters
+// # Special Characters
 
 '\n'; // Newline - Ex: "Line1\nLine2"
 '\r'; // Carriage return - Ex: "Windows line 1\r\nWindows line 2"
@@ -35,3 +35,55 @@ const s = "In JavaScript, use \\ as an escape character in strings.";
 
 '\xc9p\xe9e'; // Latin-1 character (where +XX+ is a hexadecimal Latin-1 code point)
               // "\xc9p\xe9e is fun, but foil is more fun."
+
+
+// # Template Strings
+
+let currentTemp = 19.5;
+
+// 00b0 is the Unicode code point for the "degree" symbol
+const message = `The current temperature is ${currentTemp}\u00b0C`;
+
+
+// # Multiline Strings
+
+const multiline = 'line1\
+line2';
+// output: line1line2
+
+const multiline2 = 'line1\n\
+line2';
+// output: line1
+//         line2
+
+const multiline3 = `line1
+line2`;
+// output: line1
+//         line2
+
+// But:
+const multiline3_1 = `line1
+  line2`;
+// output: line1
+//          line2
+
+// With string concatenation:
+const multiline4 = 'line1\n' +
+  'line2\n' +
+  'line3';
+// output: line1
+//         line2
+//         line3
+
+const multiline5 = 'Current temperature:\n' +
+  `\t${currentTemp}\u00b0C\n` +
+  "Don't worry...the heat is on!";
+// output: Current temperature:
+//                 19.5°C
+//         Don't worry...the heat is on!
+
+
+// # Number as string
+
+const result1 = 3 + '30'; // 3 is converted to a string; result is string '330' 
+const result2 = 3 * '30'; // '30' is converted to a number; result is numeric 90
